95b4cea4116544c3c012b794b36dc18ea6a0c3e9
SereSant <sere.santurion12@gmail.com>
2024-08-29 22:52:40 -0300

    Modificacion scrip.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,12 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    
+      const dataArray = data.students
+    showData(dataArray)
+  })
